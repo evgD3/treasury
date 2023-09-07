821ce440be4bda866d33d89e42c3bd770355cde6
ejix <ejix@zp1.net>
2023-09-07 20:13:41 +0700

    repetitive code removed

--- treasury_cli.py
+++ treasury_cli.py
@@ -7,7 +7,6 @@ from database.sqlite3 import init_db
 from database.sqlite3 import select_all
 from database.sqlite3 import select_by_category
 from database.sqlite3 import select_by_date
-from database.sqlite3 import select_groups
 
 from printer import print_all, print_stats
 from printer import print_by_date
@@ -19,16 +18,17 @@ def command(argv: list) -> None:
     conn, cur = init_db()
 
     try:
-        action = argv[1]
+        account = argv[1]
+        action = argv[2]
     except IndexError:
+        account = input('account > ').strip()
         action = input('action > ').strip()
 
     if action == '-p':
-        print_resent(select_all(cur, 'main'))
+        print_resent(select_all(cur, account))
 
     elif action == '-a':
         try:
-            account = argv[2]
             amount = int(argv[3])
             category = argv[4]
             try:
@@ -36,15 +36,13 @@ def command(argv: list) -> None:
             except IndexError:
                 comment = None
         except IndexError:
-            account = input('account> ').strip()
-            amount = int(input('amount> ').strip())
-            category = input('category> ').strip()
-            comment = input('comment >').strip()
+            amount = int(input('amount > ').strip())
+            category = input('category > ').strip()
+            comment = input('comment > ').strip()
         add_transaction(conn, cur, account, amount, category, comment)
 
     elif action == '-e':
         try:
-            account = argv[2]
             transaction_id = argv[3]
             amount = argv[4]
             category = argv[5]
@@ -53,28 +51,21 @@ def command(argv: list) -> None:
             except IndexError:
                 comment = None
         except IndexError:
-            account = input('account> ').strip()
             transaction_id = int(input('id> ').strip())
-            amount = int(input('amount> ').strip())
-            category = input('category> ').strip()
-            comment = input('comment> ').strip()
+            amount = int(input('amount > ').strip())
+            category = input('category > ').strip()
+            comment = input('comment > ').strip()
         edit_transaction(conn, cur, account, transaction_id, amount,
                          category, comment)
 
     elif action == '-pc':
         try:
-            account = argv[2]
             category = argv[3]
         except IndexError:
-            account = input('account> ').strip()
-            category = input('category> ').strip()
+            category = input('category > ').strip()
         print_category(select_by_category(cur, account, category))
 
     elif action == '-pm':
-        try:
-            account = argv[2]
-        except IndexError:
-            account = input('account >').strip()
         now = datetime.date.today()
         year = now.year
         month = now.month
@@ -92,39 +83,23 @@ def command(argv: list) -> None:
         print_by_date(select_by_date(cur, account, from_date, to_date))
 
     elif action == '-py':
-        try:
-            account = argv[2]
-        except IndexError:
-            account = input('account >').strip()
         year = datetime.date.today().year
         from_date = datetime.date.fromisoformat(f'{year}-01-01')
         to_date = datetime.date.fromisoformat(f'{year+1}-01-01')
         print_by_date(select_by_date(cur, account, from_date, to_date))
 
     elif action == '-pa':
-        try:
-            account = argv[2]
-        except IndexError:
-            account = input('account >').strip()
         print_all(select_all(cur, account))
 
     elif action == '-ps':
-        try:
-            account = argv[2]
-        except IndexError:
-            account = input('account >').strip()
-        from_date = input('from (yyyy-mm-dd)> ').strip()
+        from_date = input('from (yyyy-mm-dd) > ').strip()
         from_date = datetime.date.fromisoformat(from_date)
-        to_date = input('to (yyyy-mm-dd)> ').strip()
+        to_date = input('to (yyyy-mm-dd) > ').strip()
         to_date = datetime.date.fromisoformat(to_date)
         print_stats(select_by_date(cur, account, from_date, to_date),
                     from_date, to_date)
 
     elif action == '-pms':
-        try:
-            account = argv[2]
-        except IndexError:
-            account = input('account >').strip()
         now = datetime.date.today()
         year = now.year
         month = now.month
@@ -143,10 +118,6 @@ def command(argv: list) -> None:
                     from_date, to_date)
 
     elif action == '-pys':
-        try:
-            account = argv[2]
-        except IndexError:
-            account = input('account >').strip()
         year = datetime.date.today().year
         from_date = datetime.date.fromisoformat(f'{year}-01-01')
         to_date = datetime.date.fromisoformat(f'{year+1}-01-01')
@@ -154,10 +125,6 @@ def command(argv: list) -> None:
                     from_date, to_date)
 
     elif action == '-pas':
-        try:
-            account = argv[2]
-        except IndexError:
-            account = input('account >').strip()
         from_date = datetime.date.fromisoformat('1970-01-01')
         to_date = datetime.date.today()
         print_stats(select_all(cur, account), from_date, to_date)
