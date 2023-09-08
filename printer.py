e1e46d965c3daa9f82e44dd0ec7831017aa76e3b
ejix <ejix@zp1.net>
2023-09-08 19:45:49 +0700

    fix print_resent in printer

--- printer.py
+++ printer.py
@@ -3,12 +3,16 @@ import datetime
 
 def print_resent(transactions: list) -> None:
     transactions = transactions[:10]
-    print(f'\033[34m[{"id":<5}][{"amount":<7}][{"date":<11}][{"type":<15}]')
+    print(f'\033[34m[{"id":<5}][{"CUR":<4}][{"amount":<9}]'
+          f'[{"date":<11}][{"type":<15}]')
     for i in transactions:
         if i[2] > 0:
-            print(f'\033[92m[{i[0]:<5}][{i[1]:<7}][{i[2]} ][{i[3]:<15}]')
+            print(f'\033[92m[{i[0]:<5}][{i[1]:<4}]'
+                  f'[{i[2]:<9}][{i[3]} ][{i[4]:<15}]')
         else:
-            print(f'\033[31m[{i[0]:<5}][{i[1]:<7}][{i[2]} ][{i[3]:<15}]')
+            print(f'\033[31m[{i[0]:<5}][{i[1]:<4}]'
+                  f'[{i[2]:<9}][{i[3]} ][{i[4]:<15}]')
+
 
 
 def print_all(account: str, transactions: list) -> None:
